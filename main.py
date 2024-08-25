351772d5ff7ecb6e8c2b1fd6d8048952bf95be21
Eric Cañas <elcorreodeharu@gmail.com>
2024-08-25 10:53:20 +0200

    Use python-slugify instead of slugify to avoid a problem

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from pipeline.pipeline import Pipeline
 
 
 if __name__ == '__main__':
-    Pipeline(output_folder='youtube_channels/Mitologia/nordica/vali').generate_video()
+    Pipeline(output_folder='youtube_channels/Mitologia/kiwi/rongo').generate_video()
