0b6f4196a076c178bb9d3dfc4e8b78872c74c9db
El Yonko bravo <CPespinar@gmail.com>
2025-07-21 12:08:23 +0200

    refactor: correct comment spelling for multithreaded generation in main_fanvue_typer.py

--- utils/exceptions.py
+++
@@ -1,5 +0,0 @@
-class WaitAndRetryError(Exception):
-    def __init__(self, message, suggested_wait_time: int = 60 * 60):
-        self.message = message
-        self.suggested_wait_time = suggested_wait_time
-        super().__init__(self.message)
