c4a0ef92bfe8cb79c6bce961005f9695ada3b8a1
Moises Perez <mperesp1990@gmail.com>
2026-01-29 16:27:45 +0100

    fix: fix user object key references in session token handling

--- fanvue-fastapi/main.py
+++ fanvue-fastapi/main.py
@@ -36,8 +36,8 @@ async def home(request: Request):
         # If session was updated (token refreshed), we'd need to set new cookie
         # For simplicity, just show user info
         if user:
-            username = user.get("username", "User")
-            user_id = user.get("id", "N/A")
+            username = user.get("displayName", "User")
+            user_id = user.get("uuid", "N/A")
             return f"""
             <html>
                 <body>
